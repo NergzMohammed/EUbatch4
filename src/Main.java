72947e91fccc5a7baee8784180c4f0e447897f89
Nergz Mohmmed <72860630+NergzMohammed@users.noreply.github.com>
2021-02-01 04:41:25 +0000

    Added one motivational sentence

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world");
+
+        //I shall be very good in coding one day hopefully
     }
 }
