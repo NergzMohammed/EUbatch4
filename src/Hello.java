61c7e39ab6910f226475fef87031a7981a305dcb
Nergz Mohmmed <72860630+NergzMohammed@users.noreply.github.com>
2021-02-01 22:53:19 +0000

    Added bunch of sentences in hello and time to push to remote repo

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
 
     //Hello there
-    // I am coping with the difficult situation 
+    // I am coping with the difficult situation
+    //I have already published on github.com
+    //now I am ready to make more commits
+    //but do I need to share again ?
+    // absolutely no
+
+    //its time to commit and push
 }
